1e23265f9fa0468dd2a1b0ecec3f99f45337df4c
CHM <435469015@qq.com>
2021-04-10 15:53:39 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+
+setup(
+        name = "miki",
+        version = "2.0",
+        packages = find_packages(),
+        zip_safe = False,
+
+        description = "A Platform",
+        long_description = "A Quantitative-research Platform",
+        author = "chenhaomang",
+        author_email = "435469015@qq.com",
+
+        license = "MIT License",
+        platforms = "Independant",
+        url = "",
+        )
