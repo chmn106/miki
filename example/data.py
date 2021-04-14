f0fb0d3f9899a42aeb5dfeaa390efb20f521532f
CHM <435469015@qq.com>
2021-04-14 16:39:24 +0800

    Update data.py

--- example/data.py
+++ example/data.py
@@ -1,4 +1,6 @@
 from miki.data.dataSchedule import MikiData
+from miki.data.dataBcolz import DataBcolz
+from miki.data.dataApi import DataApi
 from miki.data import dataGlovar
 from jqdatasdk import *
 import os, redis
@@ -11,5 +13,9 @@ if __name__ == '__main__':
 	os.makedirs(dataGlovar.DataPath, exist_ok=True)
 	m = MikiData()
 	m.run()
-
-	
+	# # 下载数据
+	# start,end = '2021-01-01','2021-04-01'
+	# api = DataApi()
+	# security_list = api.get_all_securities() # 股票、指数数据
+	# d = DataBcolz()
+	# d.check_old_data(start, end, security_list)
